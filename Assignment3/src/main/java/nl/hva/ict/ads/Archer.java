50738adcf9b4d4f147c338f874505c86e9526ebe
Saif Rashed <saif.rashed@hva.nl>
2021-11-02 14:53:53 +0100

    [Assignment 3] Archer class, SelectionSort, QuickSort

--- Assignment3/src/main/java/nl/hva/ict/ads/Archer.java
+++ Assignment3/src/main/java/nl/hva/ict/ads/Archer.java
@@ -1,15 +1,20 @@
 package nl.hva.ict.ads;
 
+import java.util.Arrays;
+
 public class Archer {
     public static int MAX_ARROWS = 3;
     public static int MAX_ROUNDS = 10;
 
+    public static int archerID = 135787; // increases with each new instance
 
-    private int id;         // TODO Once assigned a value is not allowed to change.
+    private final int id;         // TODO Once assigned a value is not allowed to change.
     private String firstName;
     private String lastName;
 
     // TODO add instance variable(s) to track the scores per round per arrow
+
+    int[][] score = new int[20][3];
 
     /**
      * Constructs a new instance of Archer and assigns a unique id to the instance.
@@ -17,45 +22,95 @@ public class Archer {
      * The first instance created should have ID 135788;
      *
      * @param firstName the archers first name.
-     * @param lastName the archers surname.
+     * @param lastName  the archers surname.
      */
     public Archer(String firstName, String lastName) {
         // TODO initialise the new archer
         //  generate and assign an new unique id
         //  initialise the scores of the archer
+
+        this.id = Archer.archerID++;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     /**
      * Registers the points for each of the three arrows that have been shot during a round.
      *
-     * @param round the round for which to register the points. First round has number 1.
+     * @param round  the round for which to register the points. First round has number 1.
      * @param points the points shot during the round, one for each arrow.
      */
     public void registerScoreForRound(int round, int[] points) {
         // TODO register the points into the archer's data structure for scores.
+
+        score[round] = points.clone();
     }
 
 
     /**
      * Calculates/retrieves the total score of all arrows across all rounds
+     *
      * @return
      */
     public int getTotalScore() {
         // TODO calculate/get the total score that the archer has earned across all arrows of all registered rounds
 
-        return 0;
+        int sumScore = 0;
+
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[i].length; j++) {
+                sumScore += score[i][j];
+            }
+        }
+
+        return sumScore;
+    }
+
+    public int getTotalMisses() {
+        int sumMisses = 0;
+
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[i].length; j++) {
+                if(score[i][j] == 0) {
+                    sumMisses++;
+                }
+            }
+        }
+
+        return sumMisses;
     }
 
     /**
      * compares the scores/id of this archer with the scores/id of the other archer according to
      * the scoring scheme: highest total points -> least misses -> earliest registration
      * The archer with the lowest id has registered first
-     * @param other     the other archer to compare against
-     * @return  negative number, zero or positive number according to Comparator convention
+     *
+     * @param other the other archer to compare against
+     * @return negative number, zero or positive number according to Comparator convention
      */
     public int compareByHighestTotalScoreWithLeastMissesAndLowestId(Archer other) {
         // TODO compares the scores/id of this archer with the other archer
         //  and return the result according to Comparator conventions
+
+        if (this.getTotalScore() < other.getTotalScore()) {
+            return 1;
+        } else if (this.getTotalScore() > other.getTotalScore()) {
+            return -1;
+        }
+
+
+        if (this.getTotalMisses() > other.getTotalMisses()) {
+            return 1;
+        } else if (this.getTotalMisses() < other.getTotalMisses()) {
+            return -1;
+        }
+
+        if (this.id > other.id) {
+            return 1;
+        } else if (this.id < other.id) {
+            return -1;
+        }
+
 
         return 0;
     }
@@ -73,4 +128,15 @@ public class Archer {
     }
 
     // TODO provide a toSting implementation to format archers nicely
+
+    @Override
+    public String toString() {
+        StringBuilder archerString = new StringBuilder();
+
+        archerString.append(id + " ");
+        archerString.append("(" + getTotalScore() + ")");
+        archerString.append(" " + firstName + " " + lastName);
+
+        return archerString.toString();
+    }
 }
--- Assignment3/src/main/java/nl/hva/ict/ads/SorterImpl.java
+++ Assignment3/src/main/java/nl/hva/ict/ads/SorterImpl.java
@@ -9,27 +9,56 @@ public class SorterImpl<E> implements Sorter<E> {
      * Sorts all items by selection or insertion sort using the provided comparator
      * for deciding relative ordening of two items
      * Items are sorted 'in place' without use of an auxiliary list or array
+     *
      * @param items
      * @param comparator
-     * @return  the items sorted in place
+     * @return the items sorted in place
      */
     public List<E> selInsSort(List<E> items, Comparator<E> comparator) {
         // TODO implement selection or insertion sort
 
+        int n = items.size();
+
+        // Verplaats grens stapsgewijs vopr ongesorteerde subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Zoek het minimumelement in ongesorteerde array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+                if (comparator.compare(items.get(j), items.get(minIndex)) < 0)
+                    minIndex = j;
+
+            // Verwissel het gevonden minimumelement met het eerste element
+            swap(items, minIndex, i);
+        }
+
         return items;
+    }
+
+    /**
+     * Swap method
+     *
+     * @param items list where swap occurs
+     * @param i     First index
+     * @param j     Second index
+     */
+    public void swap(List<E> items, int i, int j) {
+        E temp = items.get(i);
+        items.set(i, items.get(j));
+        items.set(j, temp);
     }
 
     /**
      * Sorts all items by quick sort using the provided comparator
      * for deciding relative ordening of two items
      * Items are sorted 'in place' without use of an auxiliary list or array
+     *
      * @param items
      * @param comparator
-     * @return  the items sorted in place
+     * @return the items sorted in place
      */
     public List<E> quickSort(List<E> items, Comparator<E> comparator) {
         // sort the complete list of items from position 0 till size-1, encluding position size
-        this.quickSortPart(items, 0, items.size()-1, comparator);
+        this.quickSortPart(items, 0, items.size() - 1, comparator);
         return items;
     }
 
@@ -40,13 +69,50 @@ public class SorterImpl<E> implements Sorter<E> {
      *
      * @param items
      * @param comparator
-     * @return  the items sorted in place
+     * @return the items sorted in place
      */
     private void quickSortPart(List<E> items, int from, int to, Comparator<E> comparator) {
 
         // TODO quick sort the sublist of items between index positions 'from' and 'to' inclusive
 
+        if (to <= from) return;
+
+        int pivotIndex = partition(items, from, to, comparator);
+
+        // sort left half
+        quickSortPart(items, from, pivotIndex - 1, comparator);
+
+        //sort right half
+        quickSortPart(items, pivotIndex + 1, to, comparator);
+
     }
+
+    private int partition(List<E> items, int from, int to, Comparator<E> comparator) {
+
+        E pivot = items.get(from);
+        int lo = from + 1;
+        int hi = to;
+
+        do {
+
+            while (lo <= hi && comparator.compare(items.get(lo), pivot) < 0) lo++;
+            while (lo <= hi && comparator.compare(pivot, items.get(hi)) < 0) hi--;
+
+            if (lo <= hi) {
+                swap(items, lo, hi);
+
+                lo++;
+                hi--;
+            }
+
+        } while (lo <= hi);
+
+        items.set(from, items.get(hi));
+        items.set(hi, pivot);
+
+        return hi;
+    }
+
 
     /**
      * Identifies the lead collection of numTops items according to the ordening criteria of comparator
@@ -54,11 +120,12 @@ public class SorterImpl<E> implements Sorter<E> {
      * with use of (zero-based) heapSwim and heapSink operations.
      * The remaining items are kept in the tail of the list, in arbitrary order.
      * Items are sorted 'in place' without use of an auxiliary list or array or other positions in items
-     * @param numTops       the size of the lead collection of items to be found and sorted
+     *
+     * @param numTops    the size of the lead collection of items to be found and sorted
      * @param items
      * @param comparator
-     * @return              the items list with its first numTops items sorted according to comparator
-     *                      all other items >= any item in the lead collection
+     * @return the items list with its first numTops items sorted according to comparator
+     * all other items >= any item in the lead collection
      */
     public List<E> topsHeapSort(int numTops, List<E> items, Comparator<E> comparator) {
         // check 0 < numTops <= items.size()
@@ -97,7 +164,7 @@ public class SorterImpl<E> implements Sorter<E> {
         // the first numTops positions of the list now contain the lead collection
         // the reverseComparator heap condition applies to this lead collection
         // now use heapSort to realise full ordening of this collection
-        for (int i = numTops-1; i > 0; i--) {
+        for (int i = numTops - 1; i > 0; i--) {
             // loop-invariant: items[i+1..numTops-1] contains the tail part of the sorted lead collection
             // position 0 holds the root item of a heap of size i+1 organised by reverseComparator
             // this root item is the worst item of the remaining front part of the lead collection
@@ -120,8 +187,9 @@ public class SorterImpl<E> implements Sorter<E> {
      * Repairs the zero-based heap condition for items[heapSize-1] on the basis of the comparator
      * all items[0..heapSize-2] are assumed to satisfy the heap condition
      * The zero-bases heap condition says:
-     *                      all items[i] <= items[2*i+1] and items[i] <= items[2*i+2], if any
+     * all items[i] <= items[2*i+1] and items[i] <= items[2*i+2], if any
      * or equivalently:     all items[i] >= items[(i-1)/2]
+     *
      * @param items
      * @param heapSize
      * @param comparator
@@ -131,12 +199,14 @@ public class SorterImpl<E> implements Sorter<E> {
         //      i==0 || items[(i-1]/2] <= items[i]
 
     }
+
     /**
      * Repairs the zero-based heap condition for its root items[0] on the basis of the comparator
      * all items[1..heapSize-1] are assumed to satisfy the heap condition
      * The zero-bases heap condition says:
-     *                      all items[i] <= items[2*i+1] and items[i] <= items[2*i+2], if any
+     * all items[i] <= items[2*i+1] and items[i] <= items[2*i+2], if any
      * or equivalently:     all items[i] >= items[(i-1)/2]
+     *
      * @param items
      * @param heapSize
      * @param comparator
